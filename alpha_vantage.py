27867e586e8d0bd250f20063b1078aa1ff9d1e28
Aaron Raff <aaronraffdev@gmail.com>
2018-02-08 22:31:09 -0500

    Add documentation to alpha_vantage

--- alpha_vantage.py
+++ alpha_vantage.py
@@ -3,10 +3,23 @@ import ssl
 import json
 
 class AlphaVantage:
+	"""
+	Wrapper for https://www.alphavantage.co.
+	"""
 	def __init__(self, api_key):
+		"""
+		Constructs a new AlphaVantage object.
+
+		param: api_key - the api key to use for requests
+		"""
 		self.api_key = api_key
 
 	def getStockData(self, symbol):
+		"""
+		Get the stock data from the api.
+
+		param: symbol - the stock symbol to retrieve
+		"""
 		context = ssl._create_unverified_context()
 		url = ("https://www.alphavantage.co/query?" +
 			"function=TIME_SERIES_INTRADAY&symbol=" + symbol + 
@@ -16,4 +29,10 @@ class AlphaVantage:
 		print(data["Meta Data"])
 
 	def __parse(self, data):
+		"""
+		Parse JSON data.
+
+		param: data - the contents to be parsed
+		return: the parsed JSON
+		"""
 		return json.loads(data)	
