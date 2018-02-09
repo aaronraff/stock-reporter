67230d003af8f7e466e73151e95ff34d678aedc4
Aaron Raff <aaronraffdev@gmail.com>
2018-02-08 21:17:42 -0500

    Add AlphaVantage constructor

---
+++ alpha_vantage.py
@@ -0,0 +1,3 @@
+class AlphaVantage:
+	def __init__(self, api_key):
+		self.api_key = api_key
---
+++ report.py
@@ -0,0 +1,4 @@
+from config import api_key
+from alpha_vantage import AlphaVantage
+
+api = AlphaVantage(api_key)
