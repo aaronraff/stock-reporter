60a07de405bc3cc440e17f2a511d6484b65bfa60
Aaron Raff <aaronraffdev@gmail.com>
2018-02-12 22:53:13 -0500

    Reorganize GUI and add the ability to use return key to search

--- gui.py
+++ gui.py
@@ -3,16 +3,22 @@ import tkinter as tk
 def getResults(entry):
     print(entry.get())
 
+def onEnter(entry):
+    getResults(entry)
+
 root = tk.Tk()
+search = tk.Frame(root)
+search.pack(side="top")
 
-w = tk.Label(root, text="What stock are you looking for?")
-w.pack()
+w = tk.Label(search, text="What stock are you looking for?")
+w.pack(side="top")
 
-symbol = tk.Entry(root)
-symbol.pack()
+symbol = tk.Entry(search)
+symbol.pack(side="left")
 symbol.focus_set()
+symbol.bind("<Return>", lambda event: onEnter(symbol))
 
-b = tk.Button(root, text="Go", command=lambda: getResults(symbol))
+b = tk.Button(search, text="Go", command=lambda: getResults(symbol))
 b.pack(side="right")
 
 root.mainloop()
