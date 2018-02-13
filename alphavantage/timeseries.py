285ae7a56984a892325aeafae1be836cca112a7d
Aaron Raff <aaronraffdev@gmail.com>
2018-02-12 20:31:59 -0500

    Move getCurrentPrice into alphavantage package

--- alphavantage/timeseries.py
+++ alphavantage/timeseries.py
@@ -7,38 +7,54 @@ class TimeSeries:
 	Wrapper for https://www.alphavantage.co.
 	"""
 	def __init__(self, api_key):
-		"""
-		Constructs a new AlphaVantage object.
+	    """
+	    Constructs a new AlphaVantage object.
 
-		param: api_key - the api key to use for requests
-		"""
-		self.api_key = api_key
+	    param: api_key - the api key to use for requests
+	    """
+	    self.api_key = api_key
 
 	def getStockData(self, symbol):
-		"""
-		Get the stock data from the api.
+	    """
+	    Get the stock data from the api.
 
-		param: symbol - the stock symbol to retrieve
-		return the parsed JSON stock data
-		"""
-		try:
-			context = ssl._create_unverified_context()
-			url = ("https://www.alphavantage.co/query?" +
-				"function=TIME_SERIES_DAILY_ADJUSTED&symbol=" + symbol + 
-				"&apikey=" + self.api_key)
-			data = req.urlopen(url, context=context).read()
-			data = self.__parse(data)
+	    param: symbol - the stock symbol to retrieve
+	    return the parsed JSON stock data
+	    """
+	    try:
+                context = ssl._create_unverified_context()
+                url = ("https://www.alphavantage.co/query?" +
+                        "function=TIME_SERIES_DAILY_ADJUSTED&symbol=" + symbol + 
+                        "&apikey=" + self.api_key)
+                data = req.urlopen(url, context=context).read()
+                data = self.__parse(data)
 
-			return data
-		except Exception as e:
-			print("There was an error getting the data that you requested.")
-			return None
+                return data
+	    except Exception as e:
+                print("There was an error getting the data that you requested.")
+                return None
+
+
+	def getCurrentPrice(self, data):
+	    """
+	    Get the current price from the data
+	    
+	    param: data - the json data to get the current price from
+	    return: the current price from the data provided
+	    """
+	    meta = data["Meta Data"]
+	    lastUpdated = meta["3. Last Refreshed"]
+	    ts = data["Time Series (Daily)"]
+	    #get the first word (the date)
+	    date = lastUpdated.split()[0]
+	    price = ts[date]["5. adjusted close"]
+	    return price
 
 	def __parse(self, data):
-		"""
-		Parse JSON data.
+	    """
+	    Parse JSON data.
 
-		param: data - the contents to be parsed
-		return: the parsed JSON
-		"""
-		return json.loads(data)	
+	    param: data - the contents to be parsed
+	    return: the parsed JSON
+	    """
+	    return json.loads(data)     
--- report.py
+++ report.py
@@ -1,21 +1,6 @@
 from config import api_key
 from alphavantage.timeseries import TimeSeries
 
-def getCurrentPrice(data):
-	"""
-	Get the current price from the data
-
-	param: data - the json data to get the current price from
-	return: the current price from the data provided
-	"""
-	meta = data["Meta Data"]
-	lastUpdated = meta["3. Last Refreshed"]
-	ts = data["Time Series (Daily)"]
-	#get the first word (the date)
-	date = lastUpdated.split()[0]
-	price = ts[date]["5. adjusted close"]
-	return price
-
 #create new instance of AlphaVantage wrapper
 api = TimeSeries(api_key)
 
@@ -24,9 +9,10 @@ print("What stock (symbol) are you looking for?")
 symbol = input()
 
 data = api.getStockData(symbol)
+currentPrice = api.getCurrentPrice(data)
 
 #alpha_vantage.getStockData returns None if there is an error
 if data != None:
-	print(getCurrentPrice(data))
+	print(currentPrice)
 else:
 	print("There was an error getting the data.")
